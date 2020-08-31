dca9bfa84c4ec77101af4b06a70f46385b7434c5
daro <dario.hruszecki@gmail.com>
2020-08-31 14:14:12 -0300

    init

---
+++ no_supervisado.R
@@ -0,0 +1,3 @@
+library(readxl)
+raw_telecomunicaciones <- read_excel("cdatos/TPs/AID/cdatos-AID-TP/ds/Todas las bases.xlsx", 
+                              sheet = "telecomunicaciones")
---
+++ supervisado.R
@@ -0,0 +1,3 @@
+library(readxl)
+raw_accidente <- read_excel("cdatos/TPs/AID/cdatos-AID-TP/ds/Todas las bases.xlsx", 
+                              sheet = "accidente")
